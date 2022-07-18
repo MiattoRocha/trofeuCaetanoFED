f6b318faeada0f5fc68a5170f0e77c73fd84a888
gabrielaalrocha <gabriela.leme95@hotmail.com>
2022-07-18 15:19:43 -0300

    aplicacao da rota para inicializar a aplicacao na principal

---
+++ src/app/principal/principal.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { PrincipalRoutingModule } from './principal-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    PrincipalRoutingModule
+  ]
+})
+export class PrincipalModule { }
